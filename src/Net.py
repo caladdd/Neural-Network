37012e0e1958cb9419b1b7c2b2400f694dec417f
lmarinl1 <lmarinl1@outlook.com>
2018-04-22 21:41:52 -0500

    Adding str method for  Neuron Class and Net Class

--- src/Net.py
+++ src/Net.py
@@ -1,3 +1,5 @@
+import math
+
 class Neuron:
     def __init__(self):
         self.bias = 0
@@ -5,6 +7,9 @@ class Neuron:
         self.lNext = []
         self.value = 0
 
+    def __str__(self):
+        return "("+str(self.value)+")"
+        
     def addConection(self,weight,neuron):
         self.lNext.append((weight,neuron))
 
@@ -17,6 +22,7 @@ class Neuron:
     def calculateValue(self):
         # Temporally
         self.value = value
+    
 
         
 class InitialNeuron(Neuron):
@@ -35,12 +41,20 @@ class Net:
         self.initialNeurons = neurons
         self.lastNeurons = neurons
 
+    def __str__(self):
+        string = ""
+        for layer in self.schema:
+            for neuron in layer:
+                string += str(neuron)+" "
+            string+="\n"
+        return string
+    
     def addLayer(self,neurons,weightList):
         indexWeight = 0 
         for lastNeuron in self.lastNeurons:
             for neuron in neurons:
-                lastNeuron.addConection((weightList[indexWeight],neuron))
-                neuron.addConectionI((weightList[indexWeight],lastNeuron))
+                lastNeuron.addConection(weightList[indexWeight],neuron)
+                neuron.addConectionI(weightList[indexWeight],lastNeuron)
                 indexWeight += 1
         self.lastNeurons = neurons
         self.schema.append(neurons)
@@ -53,14 +67,30 @@ def main():
     n3 = InitialNeuron(0.222)
 
     neurons = [n1, n2, n3]
-    print("Se crearon las neuronas iniciales")
-
     net = Net(neurons)
-    n4 = Neuron()
-    n5 = Neuron()
-    n6 = Neuron()
-    n7 = Neuron()
-    neurons = [n4,n5,n6,n7]
+    print("Se ha creado una red")
     
+    n4 = Neuron()
+    n4.setBia(0.6)
+    n5 = Neuron()
+    n5.setBia(0.65)
+    n6 = Neuron()
+    n6.setBia(0.98)
+    n7 = Neuron()
+    n7.setBia(0.2)
+    neurons = [n4,n5,n6,n7]
+    weights = [2,3,2,3,1,4,1,4,6,7,6,7]
+    net.addLayer(neurons,weights)
+    print("Se ha agregado una capa nueva")
 
+    n8 = Neuron()
+    n8.setBia(0.8)
+    n9 = Neuron()
+    n9.setBia(0.556)
+    neurons = [n8,n9]
+    weights = [1,5,9,3,8,5,7,6]
+    net.addLayer(neurons,weights)
+
+    print(net)
+    
 main()
