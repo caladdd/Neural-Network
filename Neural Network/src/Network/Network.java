0d01012c369be6c8c4f38de1f608f38560aa80ee
caladdd <juanpablo.calad@gmail.com>
2018-04-29 21:27:57 +0000

    graphics

--- Neural Network/src/Network/Network.java
+++ Neural Network/src/Network/Network.java
@@ -1,14 +1,45 @@
 package Network;
 
-public class Network {
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Network extends JPanel{
     Neuron[][] neuron;
     
-    public Network() {
+    @Override
+    public void paintComponent(Graphics g) {
+    	super.paintComponent(g);
     	
+    	Graphics2D g2d = (Graphics2D) g;
+
+        Dimension size = getSize();
+
+        int w = size.width;
+        int h = size.height;  
+        System.out.println(w);
+    }
+    
+    
+    public void readFile() {
+    	//readFile
     }
     
     
     public static void main(String[] args) {
-    	System.out.println("a");
+    	JFrame frame = new JFrame("Neural Network");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        int width = 1200;
+        int height = 720;
+        Network network = new Network();
+        
+     // Draw the result
+        frame.add(network);
+        frame.setSize(width, height);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 }
